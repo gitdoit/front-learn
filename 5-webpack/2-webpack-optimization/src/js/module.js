6cf71d9d78816da99686526f3014065b2743f1c4
seefly <seefly@vip.qq.com>
2021-01-15 17:44:38 +0800

    优化

--- 5-webpack/2-webpack-optimization/src/js/module.js
+++ 5-webpack/2-webpack-optimization/src/js/module.js
@@ -1,5 +1,5 @@
 console.log('加载了module1~~~');
 function hello() {
-  console.log('hello~optimization##');
+  console.log('hello~optimization~~~~');
 }
 export default hello;
--- 5-webpack/2-webpack-optimization/webpack.config.js
+++ 5-webpack/2-webpack-optimization/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -14,7 +16,7 @@ module.exports = {
   devtool: 'eval-source-map',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'js/bundle.js',
+    filename: 'js/bundle.[contenthash:10].js',
   },
   plugins: [
     new HtmlWebpackPlugin(
@@ -23,24 +25,97 @@ module.exports = {
         filename: 'index.html',
       },
     ),
+    new MiniCssExtractPlugin({
+      filename: 'css/build.[contenthash:10].css',
+    }),
+    new ESLintPlugin({
+      fix: true,
+      extensions: 'js',
+    }),
   ],
   module: {
     rules: [
+      // 【eslint-loader已过期】前端各种过期，真是服了
+      // 默认命中一个loader之后如果后面还有其他loader
+      // 那么这个文件会继续test，不会breake
+      {},
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: ['postcss-preset-env'],
+        // 一个文件只会被下面多个loader命中一次
+        // 相当于循环里面的break
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader', {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    plugins: ['postcss-preset-env'],
+                  },
+                },
+              }],
+          },
+          {
+            test: /\.less$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'less-loader',
+            ],
+          },
+          {
+            test: /\.jpg|png|gif|bmp$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                name: '[hash:10].[ext]',
+                limit: 1 * 1024,
+                esModule: false,
+                outputPath: 'imgs',
+              },
             },
           },
-        }],
-      },
-      {
-        // 处理 html 中 img 资源
-        test: /\.html$/,
-        loader: 'html-loader',
+          {
+            test: /\.html$/,
+            use: ['html-loader'],
+          },
+          {
+            exclude: /\.(css|less|js|html|vue)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[hash:10].[ext]',
+              outputPath: 'imgs',
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      targets: {
+                        edge: '17',
+                        firefox: '60',
+                        chrome: '67',
+                        safari: '11.1',
+                      },
+                      useBuiltIns: 'usage',
+                      corejs: '3.6.4',
+                    },
+                  ],
+                ],
+                plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties'],
+                // 开启构建缓存，性能提升
+                cacheDirectory: true,
+              },
+            },
+          },
+        ],
       },
     ],
   },
