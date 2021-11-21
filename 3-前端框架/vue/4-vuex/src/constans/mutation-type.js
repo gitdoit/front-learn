2802bdb6538f27ae762f5c403f14d7afb8eac1d5
seefly <seefly@vip.qq.com>
2021-11-21 23:04:53 +0800

    结构调整

--- 3-前端框架/vue/4-vuex/src/constans/mutation-type.js
+++
@@ -1,7 +0,0 @@
-/* 最佳实践，定义常量API */
-export const ADD_TODO = 'add'
-export const DELETE_TODO = 'deleteItem'
-export const FINISH_TODO = 'done'
-export const CLEAR_FINISHED = 'clearFinised'
-export const CLEAR_DELETED = 'clearDeleted'
-export const CLEAR_UNDO = 'clearUndo'
--- 6-typescript/README.md
+++
@@ -1,2 +0,0 @@
-# TypeSecript学习
-> 学习资源来自[typescript入门教程](http://ts.xcatliu.com/introduction/what-is-typescript.html)
